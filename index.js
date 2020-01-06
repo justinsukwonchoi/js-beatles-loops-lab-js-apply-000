c5ad5ed9dbdd709be69793419a7edc959164d89b
Justin Choi <justinsukwonchoi@gmail.com>
2020-01-06 04:19:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function theBeatlesPlay(musicians, instruments) {
   var result = [];
   
-  for (let i = 0; i < musicians.length; i++)
+  for (let i = 0; i < musicians.length; i++) {
     var musician = musicians[i];
     var instrument = instruments[i];
     
     var string = `${musician} plays ${instrument}`;
+    
     result.push(string);
-  end
+  }
   
   return result;
 }
